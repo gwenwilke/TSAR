110f42753ec3a4a7d1f55126a4e4e6b203d5da3d
gwenwilke <148148761+gwenwilke@users.noreply.github.com>
2026-02-18 21:25:10 +0100

    Update lecture_1.2_plotting_penguins.R

--- Part1-1_Basics_and_Viz/lecture_1.2_plotting_penguins.R
+++ Part1-1_Basics_and_Viz/lecture_1.2_plotting_penguins.R
@@ -1,8 +1,7 @@
 # ====================================
 # Time Series Analysis with R
 # gwendolin.wilke@fhnw.ch
-# This script contains code used in 
-# Part 1, Lecture 1.2 Plotting with ggplot and plotly
+# 1.2 Plotting with ggplot and plotly
 # ==================================== 
 
 # ====== Install and load necessary packages
@@ -146,7 +145,7 @@ ggplot(penguins,                      # unnamed argument "data"
 # ====== Some Geoms
 
 
-# == 1D
+# == 1D numerical
 
 # bill length frequency distribution (histogram)
 ggplot(data = penguins) +
@@ -165,92 +164,16 @@ ggplot(data = penguins) +
   geom_violin(mapping = aes(x = "",
                             y = bill_length_mm)) 
 
-
-# == 2D (categorical, bar chart)
-
-# bill length and year 
-ggplot(data = penguins) +
-  geom_col(mapping = aes(x = year,
-                         y = bill_length_mm)) 
-
-# bill length and sex
-ggplot(data = penguins) +
-  geom_col(mapping = aes(x = sex,
-                         y = bill_length_mm)) 
-
-# bill length and island
-ggplot(data = penguins) +
-  geom_col(mapping = aes(x = island,
-                         y = bill_length_mm))
-
-# bill length and species
-ggplot(data = penguins) +
-  geom_col(mapping = aes(x = species,
-                         y = bill_length_mm))
-
-
-# == 2D (categorical, different plots)
-
-# bill length and year 
-ggplot(data = penguins) +
-  geom_boxplot(mapping = aes(x = year,
-                         y = bill_length_mm)) # doesnt work bc year is actually not categorical
-
-# bill length and sex
-ggplot(data = penguins) +
-  geom_boxplot(mapping = aes(x = sex,
-                         y = bill_length_mm)) 
-
-# bill length and island
-ggplot(data = penguins) +
-  geom_violin(mapping = aes(x = sex,
-                             y = bill_length_mm))
-
-# bill length frequency distribution (histogram)
-ggplot(data = penguins) +
-  geom_histogram(mapping = aes(x = bill_length_mm, 
-                               color=sex)) 
-
-
-# == 2D (numerical, scatterplot)
-
-# bill length and body mass
-ggplot(data = penguins,
-       mapping = aes(x = body_mass_g,
+# you can add the quantile markers to the violin plot, 
+# and not trim the tails of the violins
+ggplot(data = penguins, 
+       mapping = aes(x = "",
                      y = bill_length_mm)) +
-  geom_point() 
-
-# bill length and body mass with trend 
-ggplot(data = penguins,
-       mapping = aes(x = body_mass_g,
-                     y = bill_length_mm)) +
-  geom_point() +
-  geom_smooth()
-
-# bill length and body mass colored by species
-ggplot(data = penguins,
-       mapping = aes(x = body_mass_g,
-                     y = bill_length_mm,
-                     color = species)) +
-  geom_point() 
-
-# bill length and body mass colored by species with trendlines per species
-ggplot(data = penguins,
-       mapping = aes(x = body_mass_g,
-                     y = bill_length_mm,
-                     color = species)) +
-  geom_point() +
-  geom_smooth()
-
-# bill length and body mass colored by species with global trendline
-ggplot(data = penguins,
-       mapping = aes(x = body_mass_g,
-                     y = bill_length_mm)) +
-  geom_point(mapping = aes(color = species)) +
-  geom_smooth()
+  geom_violin(trim = FALSE, 
+              draw_quantiles = c(0.25, 0.5, 0.75)) 
 
 
-# == Exploring categorical variables
+# == 1D categorical variables
 
 # island count
 ggplot(data = penguins) +
@@ -261,11 +184,111 @@ ggplot(data = penguins) +
   geom_bar(mapping = aes(x = species)) +
   coord_flip()
 
-# species by island
+
+# == 2D numerical 
+
+# scatterplot: bill length vs. body mass
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm)) +
+  geom_point() 
+
+# scatterplot & polynomial trendline (plottet on top of each other)
+# bill length vs. body mass 
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm)) +
+  geom_point() +
+  geom_smooth()
+
+
+# == 2D categorical 
+
+# species by island and species (heatmap of 2d bin counts)
 ggplot(data = penguins,
        mapping = aes(x = species,
                      y = island)) +
-  geom_bin2d() # heatmap of 2d bin counts
+  geom_bin2d() 
+
+
+# == 2D (categorical x, numerical y)
+
+
+# column plot: (sum of) bill length per sex
+# Note: a column plot uses actual values per category, 
+#       while a bar plot counts the number of observations per category.
+ggplot(data = penguins) +
+  geom_col(mapping = aes(x = sex,
+                         y = bill_length_mm)) 
+
+# column plot: (sum of) bill length per island
+ggplot(data = penguins) +
+  geom_col(mapping = aes(x = island,
+                         y = bill_length_mm))
+
+# column plot: (sum of) bill length per species
+ggplot(data = penguins) +
+  geom_col(mapping = aes(x = species,
+                         y = bill_length_mm))
+
+
+# boxplot per category: bill length per sex
+ggplot(data = penguins) +
+  geom_boxplot(mapping = aes(x = sex,
+                         y = bill_length_mm)) 
+
+# violin plot  per category: bill length per sex
+ggplot(data = penguins) +
+  geom_violin(mapping = aes(x = sex,
+                             y = bill_length_mm))
+
+# violin plot per category: bill length per sex (with quantiles and untrimmed tails)
+ggplot(data = penguins, 
+       mapping = aes(x = sex,
+                     y = bill_length_mm)) +
+  geom_violin(trim = FALSE,
+              draw_quantiles = c(0.25, 0.5, 0.), fill="grey") + 
+  geom_boxplot(width=0.1)
+
+
+# histogram, stacked by category: binned bill length stacked by per sex 
+# (with sex on the color channel)
+ggplot(data = penguins) +
+  geom_histogram(mapping = aes(x = bill_length_mm, 
+                               colour=sex)) 
+
+
+
+# == 3D (numerical x, numerical y, color categorical)
+
+# scatterplot: bill length vs. body mass, color by species
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point() 
+
+# scatterplot & polynomial trendline (plottet on top of each other)
+#   - scatterplot: bill length vs. body mass, color by species
+#   - polynomoial trendline: bill length vs. body mass, color by per species 
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point() +
+  geom_smooth()
+
+# scatterplot & polynomial trendline (plottet on top of each other)
+#   - scatterplot: bill length vs. body mass, color by species
+#   - polynomoial trendline: bill length vs. body mass, no color channel
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm)) +
+  geom_point(mapping = aes(color = species)) +
+  geom_smooth()
+
+
+
 
 
 # ====== Statistical transformations
@@ -276,9 +299,11 @@ ggplot(data = penguins) +
                          y = stat(count)))
 
 # island count in % of largest population
+# Note the change in the y-axes scaling: Percentages instead of counts
 ggplot(data = penguins) +
   geom_bar(mapping = aes(x = island, 
                          y = stat(count/max(count)*100))) 
+
 
 
 # ====== Mapping versus Setting
@@ -296,7 +321,7 @@ ggplot(data = penguins) +
              color = "red") 
 
 
-# ====== Facets
+# ====== 4D with Facets
 
 ggplot(data = penguins) +
   geom_point(mapping = aes(x = body_mass_g,
@@ -305,13 +330,17 @@ ggplot(data = penguins) +
   facet_wrap(~island) 
 
 
+# ====== 5D with Facets
+
 ggplot(data = penguins) +
   geom_point(mapping = aes(x = body_mass_g,
                            y = bill_length_mm,
                            color = species)) +
   facet_grid(island ~ sex) 
 
-# ====== pairs and ggpairs()
+
+
+# ====== pairs() and ggpairs()
 
 install.packages("graphics")
 library(graphics)
@@ -328,7 +357,8 @@ my_penguins_plot <- ggplot(data = penguins) +
   geom_point(mapping = aes(x = body_mass_g,
                            y = bill_length_mm,
                            color = species))
-library(Ggally)
+
+library(GGally)
 ggplotly(my_penguins_plot)
 
 
